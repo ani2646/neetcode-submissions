fb1e7377d8d95446442c43b2583a6809f2301b9b
ani2646 <147003680+ani2646@users.noreply.github.com>
2026-06-05 00:17:10 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        vector <int> frq;
+        unordered_map<int , int> hash;
+        for(int i = 0 ;i<nums.size();i++){
+           hash[nums[i]]++;
+           if(hash[nums[i]]>nums.size()/2){
+            return nums[i];
+           }
+
+
+
+        }
+
+       
+        
+    }
+};
